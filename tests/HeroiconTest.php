08b16a1eb6f56ae6f39c28bfe4a47dfc540e7764
Andréas Lundgren <1066486+adevade@users.noreply.github.com>
2025-03-04 11:47:26 +0100

    Upgrade and refactor (#1)

--- tests/HeroiconTest.php
+++ tests/HeroiconTest.php
@@ -10,17 +10,7 @@ use function PHPUnit\Framework\assertStringContainsString;
 
 class HeroiconTest extends TestCase
 {
-    public function render(string $variant, string $icon, array $attrs = [])
-    {
-        return Statamic::tag('heroicon')->params(array_merge(['variant' => $variant, 'icon' => $icon], $attrs))->fetch();
-    }
-
-    public function getSvgAsset(string $variant, string $icon)
-    {
-        return svg('heroicon-'.$variant[0].'-'.$icon)->toHtml();
-    }
-
-    public function test_can_render_heroicon_using_outline_variant()
+    public function test_can_render_heroicon_using_outline_variant(): void
     {
         $variant = 'outline';
         $icon = 'bars-3';
@@ -28,10 +18,10 @@ class HeroiconTest extends TestCase
         $render = $this->render($variant, $icon);
         $svg = $this->getSvgAsset($variant, $icon);
 
-        assertEquals(trim($render), trim($svg));
+        assertEquals($render, $svg);
     }
 
-    public function test_can_render_heroicon_using_solid_variant()
+    public function test_can_render_heroicon_using_solid_variant(): void
     {
         $variant = 'solid';
         $icon = 'bars-3';
@@ -39,10 +29,10 @@ class HeroiconTest extends TestCase
         $render = $this->render($variant, $icon);
         $svg = $this->getSvgAsset($variant, $icon);
 
-        assertEquals(trim($render), trim($svg));
+        assertEquals($render, $svg);
     }
 
-    public function test_can_render_heroicon_using_mini_variant()
+    public function test_can_render_heroicon_using_mini_variant(): void
     {
         $variant = 'mini';
         $icon = 'bars-3';
@@ -50,10 +40,21 @@ class HeroiconTest extends TestCase
         $render = $this->render($variant, $icon);
         $svg = $this->getSvgAsset($variant, $icon);
 
-        assertEquals(trim($render), trim($svg));
+        assertEquals($render, $svg);
     }
 
-    public function test_can_add_attributes_to_svg()
+    public function test_can_render_heroicon_using_micro_variant(): void
+    {
+        $variant = 'micro';
+        $icon = 'bars-3';
+
+        $render = $this->render($variant, $icon);
+        $svg = $this->getSvgAsset($variant, $icon);
+
+        assertEquals($render, $svg);
+    }
+
+    public function test_can_add_attributes_to_svg(): void
     {
         $render = $this->render('outline', 'bars-3', ['class' => 'w-6 h-6', 'title="Main menu"']);
 
@@ -61,17 +62,40 @@ class HeroiconTest extends TestCase
         assertStringContainsString('title="Main menu"', $render);
     }
 
-    public function test_can_add_dynamically_binded_attributes_to_svg()
+    public function test_can_add_dynamically_binded_attributes_to_svg(): void
     {
         $render = $this->render('outline', 'bars-3', ['x-bind:class' => "true ? 'w-6 h-6' : 'w-5 h-5'"]);
 
         assertStringContainsString('x-bind:class="true ? \'w-6 h-6\' : \'w-5 h-5\'"', $render);
     }
 
-    public function test_will_not_throw_when_icon_name_is_invalid()
+    public function test_will_not_throw_when_icon_name_is_invalid(): void
     {
         $render = $this->render('outline', 'invalid-icon-name');
 
         assertNull($render);
+    }
+
+    protected function render(string $variant, string $icon, array $attrs = []): ?string
+    {
+        return Statamic::tag('heroicon')
+            ->params([
+                'variant' => $variant,
+                'icon' => $icon,
+                ...$attrs,
+            ])
+            ->fetch();
+    }
+
+    protected function getSvgAsset(string $variant, string $icon): string
+    {
+        $variant = match ($variant) {
+            'outline' => 'o',
+            'solid' => 's',
+            'mini' => 'm',
+            'micro' => 'c',
+        };
+
+        return svg("heroicon-{$variant}-{$icon}")->toHtml();
     }
 }
