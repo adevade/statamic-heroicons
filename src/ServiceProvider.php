1f36ee3874836595549f544700b6470d7f3ab6ca
Andréas Lundgren <andreas.lundgren@halle.se>
2025-03-04 11:44:50 +0100

    Remove Antlers runtime checks

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,20 +3,10 @@
 namespace StefanGalescu\Heroicons;
 
 use Statamic\Providers\AddonServiceProvider;
-use StefanGalescu\Heroicons\Exceptions\IncorrectEngineException;
 
 class ServiceProvider extends AddonServiceProvider
 {
     protected $tags = [
         \StefanGalescu\Heroicons\Tags\Heroicon::class,
     ];
-
-    public function bootAddon()
-    {
-        $antlersEngine = config('statamic.antlers.version');
-
-        if ($antlersEngine !== 'runtime') {
-            throw new IncorrectEngineException();
-        }
-    }
 }
